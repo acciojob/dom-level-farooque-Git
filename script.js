8c77bfcce815a04210c433c78d7fd8661e124685
Farooque <121580703+farooque-Git@users.noreply.github.com>
2023-09-30 14:42:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+// script.js
+
+// Wait for the DOM to be fully loaded before accessing elements
+document.addEventListener("DOMContentLoaded", function() {
+  // Find the element with the id "level"
+  const levelElement = document.getElementById("level");
+
+  // Initialize a counter to keep track of the DOM level
+  let domLevel = 0;
+
+  // Loop through the parent nodes of the element until the document node is reached
+  let currentNode = levelElement;
+  while (currentNode !== document) {
+    currentNode = currentNode.parentNode;
+    domLevel++;
+  }
+
+  // Display the DOM level using an alert
+  alert("The level of the element is: " + domLevel);
+});
